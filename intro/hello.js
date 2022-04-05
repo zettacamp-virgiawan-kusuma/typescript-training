b1aae12047ab986b94fc0583d706d8b311517224
zettacamp-virgiawan-kusuma <virgiawan.kusuma@zettacamp.pro>
2022-04-06 01:04:48 +0700

    intro sayHello program

---
+++ intro/hello.js
@@ -0,0 +1,5 @@
+function sayHello(name) {
+    return "hello ".concat(name);
+}
+var response = sayHello("virgiawan");
+console.log(response);
---
+++ intro/hello.ts
@@ -0,0 +1,7 @@
+function sayHello(name:string) {
+    return `hello ${name}`;
+}
+
+let response: string = sayHello("virgiawan");
+
+console.log(response);
