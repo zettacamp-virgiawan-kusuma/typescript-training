daac8ed481477d6e9b8e78e1a79c72b5ec5a4222
zettacamp-virgiawan-kusuma <virgiawan.kusuma@zettacamp.pro>
2022-04-06 01:20:25 +0700

    intro to data type

--- intro/hello.js
+++
@@ -1,5 +0,0 @@
-function sayHello(name) {
-    return "hello ".concat(name);
-}
-var response = sayHello("virgiawan");
-console.log(response);
---
+++ tipedata.ts
@@ -0,0 +1,15 @@
+// bolean
+let isGraduated: boolean = true;
+
+// number
+let age: number = 20;
+
+// string
+let firstName: string = "Virgiawan";
+let lastName: string = "Kusuma";
+let fullName = `${firstName} ${lastName}`;
+
+// say hello
+let sayHelloAgain:string = `Hello ${fullName}, selamat pagi`;
+
+console.log(sayHelloAgain);
